398c29fbfa9ffaec94e1a2822011501e2334dc3e
Gustavo Orelio <oreliogustavo@gmail.com>
2022-06-13 22:17:49 -0300

    Finalizando o crud de alunos

---
+++ src/main/java/br/com/jgsolutions/gems/model/Aluno.java
@@ -0,0 +1,41 @@
+package br.com.jgsolutions.gems.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sun.istack.NotNull;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "aluno")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Getter
+@Setter
+public class Aluno implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String nome;
+
+    private String cpf;
+    private String email;
+
+    @Column(length = 4000)
+    private String observacao;
+
+}
