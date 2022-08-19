871fade941351f01998cecec9c2b890cde619ccf
Gustavo Orelio <oreliogustavo@gmail.com>
2022-08-19 19:57:11 -0300

    Adicionando banco de dados

--- src/main/java/br/com/jgsolutions/gems/model/Pessoa.java
+++ src/main/java/br/com/jgsolutions/gems/model/Pessoa.java
@@ -3,27 +3,20 @@ package br.com.jgsolutions.gems.model;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import org.springframework.data.annotation.Id;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name = "pessoa")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 @Data
 public class Pessoa implements Serializable {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -35,7 +28,7 @@ public class Pessoa implements Serializable {
 	private String cpfPessoa;
 	
 	@NotBlank
-	private Date datanascPessoa;
+	private Date dataNascPessoa;
 	
 	@NotBlank
 	private String emailPessoa;
